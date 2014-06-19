6be685856a726f2bdc619452a586b156128a27fd
nick.hanley <nick.hanley@gmail.com>
2014-06-19 15:00:55 -0700

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Include the Capybara (giant smegging rat) DSL
+  config.include Capybara::DSL
 end
