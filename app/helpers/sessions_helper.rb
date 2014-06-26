90d6c967e7cedb869ef15c1d64c9daf87888640b
nick.hanley <nick.hanley@gmail.com>
2014-06-26 13:02:06 -0700

    add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
-   def sign_in(user)
+  def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.digest(remember_token))
@@ -15,7 +15,7 @@ module SessionsHelper
     current_user.update_attribute(:remember_token,
                                   User.digest(User.new_remember_token))
     cookies.delete(:remember_token)
-    self.current_user = nil;
+    self.current_user = nil
   end
 
   def current_user=(user)
